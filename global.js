51d1160e5e4262d20d199f5626713e5b63ed5b54
ColecamC <cclougherty@ucsd.edu>
2026-04-18 21:28:28 -0700

    added global.js

---
+++ global.js
@@ -0,0 +1,14 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+    return Array.from(context.querySelectorAll(selector));
+}
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+if (currentLink) {
+  // or if (currentLink !== undefined)
+  currentLink?.classList.add('current');
+}
