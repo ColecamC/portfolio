ee25c528d7bbe1cdc3fece4cbbf024189bb27d79
ColecamC <cclougherty@ucsd.edu>
2026-04-18 22:55:16 -0700

    finished contact sheet

--- global.js
+++ global.js
@@ -4,8 +4,85 @@ function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+// let navLinks = $$('nav a');
 
-currentLink?.classList.add('current');
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  // add the rest of your pages here
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' }
+];
+
+let nav = document.createElement('nav');
+    document.body.prepend(nav);
+
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    // Create link and add it to nav
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+            <option value="light dark">Auto</option>
+			<option value="light">Light</option>
+			<option value="dark">Dark</option>
+		</select>
+	</label>`,
+);
+matchMedia("(prefers-color-scheme: dark)").matches
+
+let select = document.querySelector('.color-scheme select');
+
+if ('colorScheme' in localStorage) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+}
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value
+
+    });
+
+let form = document.querySelector('form');
+
+form?.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let data = new FormData(form);
+    let url = form.action + '?';
+    for (let [name, value] of data) {
+        url += name + '=' + encodeURIComponent(value) + '&';
+    }
+    url = url.slice(0, -1); // remove trailing &
+    location.href = url;
+});
+
