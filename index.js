7f533b0c4d2acfa6cb5b53c51905747de40bc5ff
ColecamC <cclougherty@ucsd.edu>
2026-04-25 22:01:50 -0700

    Updated Projects to JS

---
+++ index.js
@@ -0,0 +1,18 @@
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+const projectsContainer = document.querySelector('.projects');
+renderProjects(latestProjects, projectsContainer, 'h2');
+const githubData = await fetchGitHubData('ColecamC');
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+  profileStats.innerHTML = `
+        <dl>
+          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+          <dt>Joined:</dt><dd>${githubData.created_at}</dd>
+          <dt>Followers:</dt><dd>${githubData.followers}</dd>
+          <dt>Following:</dt><dd>${githubData.following}</dd>
+        </dl>
+    `;
+}
