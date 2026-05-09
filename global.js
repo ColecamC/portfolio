381e10b412f4b6fac0639fbf7187fb41c42b2bd8
ColecamC <cclougherty@ucsd.edu>
2026-05-08 23:51:40 -0700

    added meta

--- global.js
+++ global.js
@@ -17,7 +17,8 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   // add the rest of your pages here
   { url: 'contact/', title: 'Contact' },
-  { url: 'resume/', title: 'Resume' }
+  { url: 'resume/', title: 'Resume' },
+  { url: 'meta/', title: 'Meta' },
 ];
 
 let nav = document.createElement('nav');
